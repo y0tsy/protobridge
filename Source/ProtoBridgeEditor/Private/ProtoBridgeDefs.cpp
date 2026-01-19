be734f10536027a5042ca4874b1098a49a17af9c
yotsy <zobazw@gmail.com>
2026-01-19 17:15:14 +0300

    feat(defs): update consts

--- Source/ProtoBridgeEditor/Private/ProtoBridgeDefs.cpp
+++ Source/ProtoBridgeEditor/Private/ProtoBridgeDefs.cpp
@@ -1,3 +1,11 @@
 ﻿#include "ProtoBridgeDefs.h"
 
 const FString FProtoBridgeDefs::PluginName = TEXT("ProtoBridge");
+const FString FProtoBridgeDefs::ProtocExecutable = TEXT("protoc");
+const FString FProtoBridgeDefs::PluginExecutable = TEXT("bridge_generator");
+const FString FProtoBridgeDefs::GeneratedFileExtension = TEXT(".h");
+const FString FProtoBridgeDefs::ArgFileExtension = TEXT(".args");
+const FString FProtoBridgeDefs::ProtoExtension = TEXT("proto");
+const FString FProtoBridgeDefs::BinFolder = TEXT("bin");
+const FString FProtoBridgeDefs::SourceFolder = TEXT("Source");
+const FString FProtoBridgeDefs::ThirdPartyFolder = TEXT("ProtoBridgeThirdParty");
--- Source/ProtoBridgeEditor/Public/ProtoBridgeDefs.h
+++ Source/ProtoBridgeEditor/Public/ProtoBridgeDefs.h
@@ -5,4 +5,12 @@
 struct PROTOBRIDGEEDITOR_API FProtoBridgeDefs
 {
 	static const FString PluginName;
+	static const FString ProtocExecutable;
+	static const FString PluginExecutable;
+	static const FString GeneratedFileExtension;
+	static const FString ArgFileExtension;
+	static const FString ProtoExtension;
+	static const FString BinFolder;
+	static const FString SourceFolder;
+	static const FString ThirdPartyFolder;
 };
