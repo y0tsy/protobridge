a50fc6b3159737d014502264ccb57c96d41757e0
yotsy <zobazw@gmail.com>
2026-01-20 16:56:01 +0300

    feat(filesystem): create FileSystem abstraction for mocking

---
+++ Source/ProtoBridgeEditor/Public/Interfaces/IProtoBridgeFileSystem.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+
+class PROTOBRIDGEEDITOR_API IProtoBridgeFileSystem
+{
+public:
+	virtual ~IProtoBridgeFileSystem() = default;
+
+	virtual bool FileExists(const FString& Path) const = 0;
+	virtual bool DirectoryExists(const FString& Path) const = 0;
+	virtual bool MakeDirectory(const FString& Path, bool bCreateTree) = 0;
+	virtual bool DeleteFile(const FString& Path) = 0;
+	virtual void FindFiles(TArray<FString>& OutFiles, const FString& Directory, const FString& FileExtension) const = 0;
+	virtual void FindFilesRecursive(TArray<FString>& OutFiles, const FString& StartDirectory, const FString& Filename, bool Files, bool Directories) const = 0;
+	virtual bool SaveStringToFile(const FString& String, const FString& Filename) = 0;
+};
