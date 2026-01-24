da71653b11e3a636d6f29113dc8f056f671b0317
yotsy <zobazw@gmail.com>
2026-01-24 14:38:37 +0300

    build(protobridgecore): delete RTTI usage and enabling of exceptions

--- Source/ProtoBridgeCore/ProtoBridgeCore.Build.cs
+++ Source/ProtoBridgeCore/ProtoBridgeCore.Build.cs
@@ -6,9 +6,6 @@ public class ProtoBridgeCore : ModuleRules
 	public ProtoBridgeCore(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		bUseRTTI = true;
-		bEnableExceptions = true;
 
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "ProtoBridgeThirdParty", "GameplayTags", "Json" });
 		PrivateDependencyModuleNames.AddRange(new string[] { "CoreUObject", "Engine" }); 
