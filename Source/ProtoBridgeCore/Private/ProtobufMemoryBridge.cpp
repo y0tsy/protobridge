1eebbe95227ed36a83c0ce04321252db07a3099f
yotsy <zobazw@gmail.com>
2026-02-03 09:55:52 +0300

    feat(bridge-core): create memory bridge for arenas

---
+++ Source/ProtoBridgeCore/Private/ProtobufMemoryBridge.cpp
@@ -0,0 +1,25 @@
+﻿#include "ProtobufMemoryBridge.h"
+
+LLM_DEFINE_TAG(Protobuf);
+
+namespace
+{
+	void* ProtobufBlockAlloc(size_t Size)
+	{
+		LLM_SCOPE_BYTAG(Protobuf);
+		return FMemory::Malloc(Size, 16);
+	}
+
+	void ProtobufBlockDealloc(void* Ptr, size_t Size)
+	{
+		FMemory::Free(Ptr);
+	}
+}
+
+google::protobuf::ArenaOptions FProtobufMemoryBridge::GetArenaOptions()
+{
+	google::protobuf::ArenaOptions Options;
+	Options.block_alloc = &ProtobufBlockAlloc;
+	Options.block_dealloc = &ProtobufBlockDealloc;
+	return Options;
+}
---
+++ Source/ProtoBridgeCore/Public/ProtobufMemoryBridge.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "ProtobufIncludes.h"
+
+class PROTOBRIDGECORE_API FProtobufMemoryBridge
+{
+public:
+	static google::protobuf::ArenaOptions GetArenaOptions();
+};
